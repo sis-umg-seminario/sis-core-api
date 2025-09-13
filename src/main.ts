65a816a143727975206b873522f72d98a002c400
Luis Celano <luiscelano123@live.com>
2025-09-13 17:14:14 -0600

    update start script

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,6 @@ async function bootstrap() {
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
